43413fb20e5a538e5fb234a013f92405ac243e65
ishancoder <isaxena171@gmail.com>
2016-01-11 22:12:22 +0530

    Added a Jumbotron for info

--- js/app.js
+++ js/app.js
@@ -2,29 +2,43 @@ var app = angular.module("portFolio", [ ]);
 
 app.factory("projectData", function(){
 	var object = {
+		bio:{
+			msgHead:"Example!!!!",
+			msgBody:"<Enter your example message body here!>",
+			name:"Your Name",
+			gplus:"",
+			twitter:"",
+			facebook:"",
+			website:""
+		},
 		projects:[
 			{
-				imgSrc:"something",
+				imgSrc:"./resources/img.png",
 				header:"AweSome Projects",
 				description:"Some Example description replace this with your own project description."
 			},
 			{
-				imgSrc:"something",
+				imgSrc:"./resources/img.png",
 				header:"AweSome Projects",
 				description:"Some Example description replace this with your own project description."
 			},
 			{
-				imgSrc:"something",
+				imgSrc:"./resources/img.png",
 				header:"AweSome Projects",
 				description:"Some Example description replace this with your own project description."
 			},
 			{
-				imgSrc:"something",
+				imgSrc:"./resources/img.png",
 				header:"AweSome Projects",
 				description:"Some Example description replace this with your own project description."
 			},
 			{
-				imgSrc:"something",
+				imgSrc:"./resources/img.png",
+				header:"AweSome Projects",
+				description:"Some Example description replace this with your own project description."
+			},
+			{
+				imgSrc:"./resources/img.png",
 				header:"AweSome Projects",
 				description:"Some Example description replace this with your own project description."
 			}
