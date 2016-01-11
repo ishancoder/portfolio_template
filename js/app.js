f7bd0cac67dbd600e637e6e0a753021a5b1f4cd6
ishancoder <isaxena171@gmail.com>
2016-01-11 21:46:54 +0530

    Added Some Projects

--- js/app.js
+++ js/app.js
@@ -1,12 +1,38 @@
-var app = angular.module("portFolio");
+var app = angular.module("portFolio", [ ]);
 
 app.factory("projectData", function(){
 	var object = {
-
+		projects:[
+			{
+				imgSrc:"something",
+				header:"AweSome Projects",
+				description:"Some Example description replace this with your own project description."
+			},
+			{
+				imgSrc:"something",
+				header:"AweSome Projects",
+				description:"Some Example description replace this with your own project description."
+			},
+			{
+				imgSrc:"something",
+				header:"AweSome Projects",
+				description:"Some Example description replace this with your own project description."
+			},
+			{
+				imgSrc:"something",
+				header:"AweSome Projects",
+				description:"Some Example description replace this with your own project description."
+			},
+			{
+				imgSrc:"something",
+				header:"AweSome Projects",
+				description:"Some Example description replace this with your own project description."
+			}
+		]
 	};
 	return object;
 });
 
 app.controller("MainCtrl",["$scope","projectData", function($scope, projectData) {
-
+	$scope.data = projectData;
 }]);
