b4c2aa979f817b523db69f82ad6613ff64032e9b
ishancoder <isaxena171@gmail.com>
2016-01-11 21:40:24 +0530

    Created a Simple Template for PortFolio

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module("portFolio");
+
+app.factory("projectData", function(){
+	var object = {
+
+	};
+	return object;
+});
+
+app.controller("MainCtrl",["$scope","projectData", function($scope, projectData) {
+
+}]);
